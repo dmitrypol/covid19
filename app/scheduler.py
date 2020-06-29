18bdd43ebec409f51931cd497f792ad7a7ef8b1c
Dmitry Polyakovsky <dmitry.polyakovsky@oracle.com>
2020-06-28 18:04:57 -0700

    updated OKE naming convention

--- app/scheduler.py
+++ app/scheduler.py
@@ -35,8 +35,8 @@ def refresh_hp():
     try:
         my_lock = DLM.lock('refresh_hp', 10000)  #   in milliseconds
         if my_lock:
-            jobs.get_url('http://147.154.10.44')
-            jobs.get_url('http://147.154.10.44?format=json')
+            jobs.get_url('http://138.1.50.25/')
+            jobs.get_url('http://138.1.50.25?format=json')
             logging.info('refresh_hp')
             time.sleep(1)
             DLM.unlock(my_lock)
